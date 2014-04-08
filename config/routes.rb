79564a99bddf6cd8a00882e01f15139a22bf2ea5
Michael O'Keefe <okeefm@rpi.edu>
2014-04-07 23:47:03 -0400

    Added user card-specific routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 RpiaRfidRails::Application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
+  
+  get "/users/:id/edit_card_details", to: "users#edit_card_details", as: "edit_card"
+  post "/users/:id/edit_card_details", to: "users#update_card_details", as: "update_card"
 end
